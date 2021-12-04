d4f2a5beb8e5563b1880f75f60606b94ad027b45
Raphael Quiñones <rpquinones@up.edu.ph>
2021-12-05 04:04:30 +0800

    First working backend

---
+++ main.py
@@ -0,0 +1,22 @@
+import requests
+from datetime import datetime
+
+
+def getSLP(roninAdd):
+    response = requests.get("https://game-api.axie.technology/slp/" + roninAdd)
+    return response.json()
+
+
+if __name__ == '__main__':
+    roninAdd = "ronin:71dd7cb6743b94b5bffc2a6d43ccdb6d6dd98912"
+    jsonFile = getSLP(roninAdd)
+
+    claimable = jsonFile[0]["claimable_total"]
+    total = jsonFile[0]["total"]
+    lastDateClaimed = jsonFile[0]["last_claimed_item_at"]  # in UNIX timecode
+    lastDateUpdate = jsonFile[0]["update_time"]  # in UNIX timecode
+    lastDateUpdate /= 1000
+    print("Total Claimable: " + str(claimable))
+    print("Total: " + str(total))
+    print("Last Date Claimed: " + datetime.utcfromtimestamp(lastDateClaimed).strftime('%Y-%m-%d %H:%M:%S'))
+    print("Last Date Site Refreshed: " + datetime.utcfromtimestamp(lastDateUpdate).strftime('%Y-%m-%d %H:%M:%S'))
