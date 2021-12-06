d7ea4d728a0051a97f108872084a76da2cdb3529
Raphael Quiñones <rpquinones@up.edu.ph>
2021-12-07 04:51:34 +0800

    Added Ronin addresses and Name functionality

--- app.py
+++ app.py
@@ -1,41 +1,44 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from main import getSLP
 from datetime import datetime
 
 app = Flask(__name__)
 
-headings = ("Total SLP", "Last Claimed","Date Refreshed")
-
-fromAPI = getSLP("ronin:71dd7cb6743b94b5bffc2a6d43ccdb6d6dd98912,ronin:b800eea5e1ce4d49ca2db939e9883414f0bd5ff2")
-
-summary = [] #store data from each address
-for raw_data in fromAPI:
-    row = []
-
-
-    #claimable slp
-    row.append(raw_data["claimable_total"])
-
-    #lastclaimed
-    lastclaim = raw_data["update_time"]
-    lastclaim /= 1000  # converter doesn't read miliseconds UTX
-    row.append(datetime.utcfromtimestamp(lastclaim).strftime('%Y-%m-%d %H:%M:%S'))
-
-    #date
-    each_date = raw_data["update_time"]
-    each_date /= 1000 #converter doesn't read miliseconds UTX
-    row.append(datetime.utcfromtimestamp(each_date).strftime('%Y-%m-%d %H:%M:%S'))
-
-    #store into summary
-    summary.append(row)
-
-
-
-
-
-@app.route("/")
+summary = []  # store data from each address
+@app.route("/",methods=["POST", "GET"])
 def home():
+    headings = ("Name", "Total SLP", "Last Claimed", "Date Refreshed")
+    name = request.form["name"]
+    address = request.form["ronin_add"]
+
+
+    fromAPI = getSLP(address)
+
+    for raw_data in fromAPI:
+        row = []
+
+        # name
+        row.append(name)
+
+        # claimable slp
+        row.append(raw_data["claimable_total"])
+
+        # lastclaimed
+        lastclaim = raw_data["update_time"]
+        lastclaim /= 1000  # converter doesn't read miliseconds UTX
+        row.append(datetime.utcfromtimestamp(lastclaim).strftime('%Y-%m-%d %H:%M:%S'))
+
+        # date
+        each_date = raw_data["update_time"]
+        each_date /= 1000  # converter doesn't read miliseconds UTX
+        row.append(datetime.utcfromtimestamp(each_date).strftime('%Y-%m-%d %H:%M:%S'))
+
+        # store into summary
+        summary.append(row)
+
     return render_template("index.html", headings = headings, summary=summary)
+
+
 
 
 if __name__ == "__main__":
