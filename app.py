e2b82e298fe88df7f51c4670f6214ff48f320dfd
Raphael Quiñones <rpquinones@up.edu.ph>
2021-12-07 05:31:17 +0800

    Removing Confirm Form Resubmission to avoid duplicates of info - wasn't able to resolve it properly - just used checking duplicates

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from datetime import datetime
 app = Flask(__name__)
 
 summary = []  # store data from each address
+addresses = [] # check if address's already there to avoid duplicates
 @app.route("/",methods=["POST", "GET"])
 def home():
     headings = ("Name", "Total SLP", "Last Claimed", "Date Refreshed")
@@ -12,29 +13,35 @@ def home():
     address = request.form["ronin_add"]
 
 
-    fromAPI = getSLP(address)
+    if address in addresses:
 
-    for raw_data in fromAPI:
-        row = []
+        return render_template("index.html", headings = headings, summary=summary)
+    else:
+        fromAPI = getSLP(address)
+        for raw_data in fromAPI:
+            row = []
 
-        # name
-        row.append(name)
+            # name
+            row.append(name)
 
-        # claimable slp
-        row.append(raw_data["claimable_total"])
+            # claimable slp
+            row.append(raw_data["claimable_total"])
 
-        # lastclaimed
-        lastclaim = raw_data["update_time"]
-        lastclaim /= 1000  # converter doesn't read miliseconds UTX
-        row.append(datetime.utcfromtimestamp(lastclaim).strftime('%Y-%m-%d %H:%M:%S'))
+            # lastclaimed
+            lastclaim = raw_data["update_time"]
+            lastclaim /= 1000  # converter doesn't read miliseconds UTX
+            row.append(datetime.utcfromtimestamp(lastclaim).strftime('%Y-%m-%d %H:%M:%S'))
 
-        # date
-        each_date = raw_data["update_time"]
-        each_date /= 1000  # converter doesn't read miliseconds UTX
-        row.append(datetime.utcfromtimestamp(each_date).strftime('%Y-%m-%d %H:%M:%S'))
+            # date
+            each_date = raw_data["update_time"]
+            each_date /= 1000  # converter doesn't read miliseconds UTX
+            row.append(datetime.utcfromtimestamp(each_date).strftime('%Y-%m-%d %H:%M:%S'))
 
-        # store into summary
-        summary.append(row)
+            # store into summary
+            summary.append(row)
+
+            # store address for duplicate checking
+            addresses.append(address)
 
     return render_template("index.html", headings = headings, summary=summary)
 
